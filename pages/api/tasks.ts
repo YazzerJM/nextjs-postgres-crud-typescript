55655924b54ded7d9ce0e4a4c53ccb2daf0ff93c
Yasser Jimenez <yasserjimenezmartinez@hotmail.com>
2022-09-23 19:27:31 -0500

    rutas

---
+++ pages/api/tasks.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    res.json('tasks');
+}
